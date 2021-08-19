d0f94ea9d290d3762b5e6149e7e5e5cc274623bb
simit patel <simitp@gmail.com>
2021-08-18 22:08:39 -0400

    updae

--- R/factor-functions.R
+++ R/factor-functions.R
@@ -1,3 +1,37 @@
+#' @title Factor and Metric
+#'
+#' @description Report a factor and a metric.
+#'
+#' @param metric_df A dataframe
+#' @param template_id Optional. A character string.
+#'
+#' @return A sentence ready for insertion into
+#' an HTML document.
+#'
+#'@examples
+#' \dontrun{
+#'report_factor_and_metric(metric_df)
+#'}
+#'
+#' @export report_factor_and_metric
+#' @import tibble
+#' @import httr
+#' @import dplyr
+
+report_factor_and_metric <- function(metric_df,template_id = NULL) {
+  key <- Sys.getenv('table_to_text')
+
+  pb <- list(metric_df = metric_df,
+             key = key,
+             template_id = ifelse(is.null(template_id),'default',template_id))
+
+  body_json <- paste0('{"post_body":',jsonlite::toJSON(pb), '}', sep = '')
+  response <- httr::POST("https://generate-text-mrwwgrktvq-ue.a.run.app/report-factor-and-metric",
+                         body = body_json,httr::accept_json()) %>% httr::content() %>%
+    as.character()
+  return(response)
+}
+
 #' @title Report Most Common Factor
 #'
 #' @description Report which factors (categorical variables) are the
@@ -21,15 +55,10 @@
 
 report_most_common_factor <- function(metric_df,template_id = NULL) {
   key <- Sys.getenv('table_to_text')
-  if(is.null(template_id)) {
-    pb <- list(metric_df = metric_df,
-               key = key)
-  }
-  if(!is.null(template_id)) {
-    pb <- list(metric_df = metric_df,
-               key = key,
-               template_id = template_id)
-  }
+
+  pb <- list(metric_df = metric_df,
+             key = key,
+             template_id = ifelse(is.null(template_id),'default',template_id))
 
   body_json <- paste0('{"post_body":',jsonlite::toJSON(pb), '}', sep = '')
   response <- httr::POST("https://generate-text-mrwwgrktvq-ue.a.run.app/report-most-common-factors",
